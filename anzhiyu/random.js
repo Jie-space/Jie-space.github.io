453ed7e42572e086ba75be4c71e24bb065ef316e
Jie <3159593609@qq.com>
2024-07-28 20:15:49 +0800

    Site updated: 2024-07-28 20:15:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/27/吊呢了/","2024/07/27/新的文件/","2024/07/27/hello-world/"];function toRandomPost(){
+var posts=["2024/07/27/hello-world/","2024/07/27/吊呢了/","2024/07/27/新的文件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
